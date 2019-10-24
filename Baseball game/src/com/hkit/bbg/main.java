5d292eec5df2a3512f1e544d64317fe5d5c56769
USER1 <USER1@USER9-PC>
2019-10-24 11:47:08 +0900

    처음

---
+++ Baseball game/src/com/hkit/bbg/main.java
@@ -0,0 +1,20 @@
+package com.hkit.bbg;
+
+public class main {
+	public static void main(String[] args) {
+	
+		int NO_COUNT = 3;
+		
+		int[] randomNoArray = new int[NO_COUNT];
+		int[] userNoArray = new int[NO_COUNT];
+	
+		//0~0.9999999
+		int randomValue = (int)(Math.random()*9)+1;
+		//0~9
+	
+	}
+	
+	
+	
+	
+}
